41e20cc52ce027850b9420421720484722d6b87f
david silva <david_silva-4@hotmail.com>
2014-12-02 13:58:44 +0100

    casos de prueba

--- src/Empresa.py
+++ src/Empresa.py
@@ -1,6 +1,7 @@
 __author__ = 'David'
 
-
+from Empleado import *
+from Departamento import *
 class Empresa():
     def __init__(self, nombre_empresa, cif, razon_social):
         """
@@ -15,3 +16,11 @@ class Empresa():
         self.cif = cif
         self.rs = razon_social
         self.lDepartamentos = []
+    def nuevoDept(self, dep):
+        self.lDepartamentos.append(dep)
+
+emp=Empleado( "david", "silva","49028333s", "c/catania nº15", 24, "david@msn.es", 2500)
+dep=Departamento("Informatica", 1)
+dep.intro_empleado(emp)
+emp=Empresa("Epd7","492015764p","SA")
+emp.nuevoDept(dep)
